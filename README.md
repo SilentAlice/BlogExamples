8283664fc972128b4d3cfd5536729df43badd47e
SilentAlice <yumingwu233@gmail.com>
2016-12-28 15:01:07 +0800

    Kernel Module

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Posts
 See posts on [SilentMing's Gensokyo][1]. Most of them are written in **Chinese**, while you can leave comments or send me emails in **English** or **Janpanese** as well.
 
-Xen\_Log\_2 - Build HVM Domain: Xen Config files
-Xen\_Log\_3 - Add New Hypercall to Xen: Kernel module code
+* `Xen_Log_2` - Build HVM Domain: Xen Config files
+* `Xen_Log_3` - Add New Hypercall to Xen: Kernel module code
 
 
 
---
+++ kernel_module/alice_module.c
@@ -0,0 +1,33 @@
+/* Demo: Kernel Module 
+ * Post: http://silentming.net/blog/2016/12/28/kernel-module/
+ * This is kernel module under GPL License
+ *
+ * Compile:
+ * make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
+ *
+ * Run:
+ * insmod alice_module.ko
+ */
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/types.h>
+
+static int init_alice(void)
+{
+    
+    pr_info("--------->Hello, This is Alice\n");
+    return 0;
+}
+
+static void exit_alice(void)
+{
+   pr_info("================================>Exit Successfully\n");
+}
+module_init(init_alice);
+module_exit(exit_alice);
+
+MODULE_LICENSE("GPL");
+
+
+
