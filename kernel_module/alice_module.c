c4b848858f0ea30c791efe69073747461a259ec3
SilentAlice <yumingwu233@gmail.com>
2018-06-13 16:31:04 +0800

    Add intro-to-virt slides00

--- kernel_module/alice_module.c
+++ kernel_module/alice_module.c
@@ -9,25 +9,46 @@
  * insmod alice_module.ko
  */
 
+#define HASPARAM 1
+
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/types.h>
+#if HASPARAM
+#include <linux/moduleparam.h>
+
+static int arg1;
+static int arg2;
+#endif
 
 static int init_alice(void)
 {
     
     pr_info("--------->Hello, This is Alice\n");
+#if HASPARAM
+    pr_info("param arg1:%d, arg2:%d\n", arg1, arg2);
+#endif
+
     return 0;
+
 }
 
 static void exit_alice(void)
 {
    pr_info("================================>Exit Successfully\n");
 }
+
 module_init(init_alice);
 module_exit(exit_alice);
 
+#if HASPARAM
+module_param(arg1, int, 0644); 
+module_param(arg2, int, 0644); 
+#endif
+
 MODULE_LICENSE("GPL");
+MODULE_AUTHOR("SilentAlice");
+MODULE_DESCRIPTION("Simple module demo");
 
 
 
